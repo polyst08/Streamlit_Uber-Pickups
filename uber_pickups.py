c948a86755a402e798d84c4bf7305e432c521c97
polyst08 <ploymwit555@gmail.com>
2025-11-02 10:24:33 +0700

    Update title and formatting in uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickup in NYC by');
+st.title('Uber pickup in NYC by P');
  
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
@@ -37,3 +37,4 @@ hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Map of all pickups at {hour_to_filter}:00')
 st.map(filtered_data)
+
